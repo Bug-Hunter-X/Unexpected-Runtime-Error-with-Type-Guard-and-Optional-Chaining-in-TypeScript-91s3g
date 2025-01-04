355cdb4c18124f1d38f06bbdbe39bf3e1e211746
BugHunter-X <askzenai@gmail.com>
2025-01-04 14:40:05 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,11 @@
+function printName(name: string | null): void {
+  if (name) {
+    console.log(name.toUpperCase());
+  } else {
+    console.log('No name provided');
+  }
+}
+
+printName(null); // Output: No name provided
+printName('John'); // Output: JOHN
+printName(undefined); // Error!
